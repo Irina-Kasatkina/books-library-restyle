1b26201aae9e204667911341f71ca3d4b236b52c
Irina Kasatkina <kasatkina235@gmail.com>
2022-09-07 15:14:37 +0300

    add HTTPError handling

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -41,7 +41,29 @@ def create_parser():
     return parser
 
 
-def get_page_books_urls(category_page_url):
+def get_books_urls(start_page, end_page):
+    """ Получает полные url книг из указанного диапазона страниц каталога фантастики. """
+
+    real_end_page = get_real_endpage()
+    if start_page > real_end_page:
+        logger.warning(
+            f'Запрошены страницы {start_page}-{end_page}, '
+            f'но в каталоге всего {real_end_page} страниц.'
+        )
+        return []
+
+    end_page = min(end_page, real_end_page)
+    books_urls = []
+    for page_number in range(start_page, end_page+1):
+        category_page_url = f'https://tululu.org/l55/{page_number}/'
+        page_books_urls = get_parsed_category_page(category_page_url)['books_urls']
+        books_urls.extend([urljoin(category_page_url, book_url) for book_url in page_books_urls])
+        page_number += 1
+
+    return books_urls
+
+
+def get_parsed_category_page(category_page_url):
     """ Получает полные url книг из указанной страницы каталога фантастики. """
 
     while True:
@@ -50,6 +72,11 @@ def get_page_books_urls(category_page_url):
             response.raise_for_status()
             check_for_redirect(response)
             return parse_category_page(response.content)
+        except requests.HTTPError:
+            logger.warning(
+                'Возникла ошибка HTTPError. '
+                f'Возможно, страница каталога {category_page_url} отсутствует на сайте.'
+            )
         except requests.exceptions.ConnectionError:
             logger.warning(
                 f'При загрузке страницы {category_page_url} '
@@ -57,23 +84,16 @@ def get_page_books_urls(category_page_url):
             )
             time.sleep(QUERY_TIMEOUT)
             continue
+        break
     return None
 
 
-def get_books_urls(start_page, end_page):
-    """ Получает полные url книг из указанного диапазона страниц каталога фантастики. """
+def get_real_endpage():
+    """ Получает с сайта реальное количество страниц каталога. """
 
-    books_urls = []
-    for page_number in range(start_page, end_page+1):
-        try:
-            category_page_url = f'https://tululu.org/l55/{page_number}/'
-            page_books_urls = get_page_books_urls(category_page_url)
-            books_urls.extend([urljoin(category_page_url, book_url) for book_url in page_books_urls])
-        except requests.HTTPError:
-            break
-        page_number += 1
-
-    return books_urls
+    page_number = 1
+    category_page_url = f'https://tululu.org/l55/{page_number}/'
+    return get_parsed_category_page(category_page_url)['pages_count']
 
 
 def parse_category_page(response_content):
@@ -84,7 +104,9 @@ def parse_category_page(response_content):
     books_tags = soup.select('.d_book')
     books_urls = [book_tag.select_one('a')['href'] for book_tag in books_tags]
 
-    return books_urls
+    pages_count = int(soup.select('a.npage')[-1].text)
+
+    return {'books_urls': books_urls, 'pages_count': pages_count}
 
 
 def main():
@@ -104,12 +126,13 @@ def main():
         end_page = start_page
 
     books_urls = get_books_urls(start_page, end_page)
-    books_details = download_books(books_urls, dest_folder=args.dest_folder,
-                                   skip_txt=args.skip_txt, skip_imgs=args.skip_imgs)
+    if books_urls:
+        books_details = download_books(books_urls, dest_folder=args.dest_folder,
+                                       skip_txt=args.skip_txt, skip_imgs=args.skip_imgs)
 
-    json_path = args.json_path if args.json_path else 'books_details.json'
-    with open(json_path, 'w', encoding='utf8') as json_file:
-        json.dump(books_details, json_file, ensure_ascii=False, indent=4)
+        json_path = args.json_path if args.json_path else 'books_details.json'
+        with open(json_path, 'w', encoding='utf8') as json_file:
+            json.dump(books_details, json_file, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
